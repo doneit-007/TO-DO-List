d96ab3b76854d042b7e532dae3366d1172415f69
Nitesh Gupta <nitesh.gupta.ece19@itbhu.ac.in>
2021-10-09 23:46:39 +0530

    TO DO LIST DONE

--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Item extends React.Component{
+constructor(props){
+  super(props);
+
+  this.state={show:true,};
+
+
+ }
+ 
+render(){
+   console.log("agya");
+     if(this.state.show===false)
+    	return null;
+   return(
+   
+   <li className = "listitem">
+    
+   <div className ="messagebox" >
+   <span className = "List">
+   	{this.props.message}
+   </span>
+   <button className = "delete" onClick={()=> this.setState({show:false})}>DONE</button>
+   </div>
+     </li>
+   	)
+
+
+}
+}
+function ReturnList(props) {
+ 
+
+  return (
+    <ul>
+      {props.list.map((items) => (
+        <Item message={items}/>
+      ))}
+    </ul>
+  );
+}
+class Additem extends React.Component{
+constructor(props){
+super(props);
+
+this.state = {
+  message : '',
+  list : [],
+ 
+}
+
+this.updateInput = this.updateInput.bind(this);
+this.renderitem = this.renderitem.bind(this);
+}
+
+
+updateInput(event){
+this.setState({message: event.target.value})
+}
+ 
+renderitem(mes)
+ {  
+      
+       let listitem = this.state.list;
+
+// Add item to it
+listitem.push(mes);
+
+// Set state
+this.setState({list:listitem});
+
+    /*
+    console.log(this.state.list.length);*/
+ }
+
+
+render(){
+   return(
+   	<div className = "todolist">
+  <div className = "inputbox">
+   <form >
+   <input id ="message" type="text" onChange={this.updateInput}/>
+   </form>
+   
+   <button className="Add" onClick={()=>this.renderitem(this.state.message)}>ADD TO LIST</button>
+   </div>
+   <div className="todo">
+     <h1 align = "center">TODO LIST</h1>
+     <div>
+       <ReturnList list ={this.state.list}/>
+     </div>
+     </div>
+     </div>
+   	)
+
+
+}
+
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Additem/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
